9c662e55e8c5720ce366c8618785012c152e0a12
Nadiia <npodufala15@gmail.com>
2021-02-13 14:14:51 +0200

    typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
+  if(array == null || array.length == 0)
   return 0;
-}
+  let min = array[0];
+  for(let i =1; i< array.length; i++){
+if(array[i] < min)
+min = array[i];
+  }
+  return min;
+};
+  
+
 
 exports.max = function max (array) {
+  if(array == null || array.length == 0)
   return 0;
-}
+  let max = array[0];
+  for(let i =1; i< array.length; i++){
+if(array[i] > max)
+max = array[i];
+  }
+  return max;};
 
 exports.avg = function avg (array) {
+  if(array == null || array.length == 0)
   return 0;
-}
+  let sum = 0;
+  for(let i =0; i< array.length; i++){
+    sum += array[i];
+  }
+let avg = 0;
+  return avg = sum / array.length;
+};
